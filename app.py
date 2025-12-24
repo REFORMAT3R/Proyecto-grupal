95dc99150a556482c763b85ce6383c7f7378b419
Luis <lchoquepuma@unsa.edu.pe>
2025-12-24 04:19:50 -0600

    Archivo requeriments

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for
+import psycopg2
+import os
 
 app = Flask(__name__)
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
+def get_db_connection():
+    return psycopg2.connect(DATABASE_URL)
+
+# Página principal
 @app.route('/')
 def nuevoindex():
     return render_template('nuevoindex.html')
 
+#Página de trajes folklóricos
 @app.route('/trajes')
 def trajes():
     return render_template('trajes.html')
 
+# Página de contacto
 @app.route('/contacto')
 def contacto():
     return render_template('contacto.html')
 
+#Páginas de trajes específicos
 @app.route('/wititi')
 def wititi():
     return render_template('wititi.html')
@@ -42,9 +52,33 @@ def montonero():
 @app.route('/caporales')
 def caporales():
     return render_template('caporales.html')
-@app.route('/formulario')
+
+#Formulario de reserva de trajes
+@app.route('/formulario', methods=['GET', 'POST'])
 def formulario():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        telefono = request.form['telefono']
+        traje = request.form['traje']
+        mensaje = request.form['mensaje']
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute("""
+            INSERT INTO reservas (nombre, correo, telefono, traje, mensaje)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (nombre, correo, telefono, traje, mensaje))
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return redirect(url_for('nuevoindex'))
+
     return render_template('formulario.html')
+
 
 if __name__ == "__main__":
     app.run()
